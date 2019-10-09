2da6211c42f3157e729fac8fcd60096fbe75b40c
Jisie DM <jisie.david@gmail.com>
2019-10-09 10:47:34 -0500

    Code from the chapter

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,38 @@
 console.log("This is working!!");
+
+/*
+    This code will only return the first section component
+    even though there are three components with the same
+    class
+*/
+const sectionEl = document.querySelector(".article__section");
+console.log("sectionEl", sectionEl);
+
+/*
+    This code will only return the third section component
+*/
+const footerEl = document.querySelector(".article__footer");
+console.log("footerEl", footerEl);
+
+/*
+    Get reference to the section with the class of `smallText`
+    and remove that class. Then add a new class of `largeText`
+*/
+const smallTextEl = document.querySelector(".smallText");
+console.log("Get reference to element with class smallText", smallTextEl);
+debugger
+smallTextEl.classList.remove("smallText")
+console.log("Removed class smallText", smallTextEl);
+debugger
+smallTextEl.classList.add("largeText")
+console.log("Added class largeText", smallTextEl);
+
+/*
+    This code will return all section components
+*/
+const sectionEl = document.querySelectorAll(".article__section");
+console.log("sectionEl", sectionEl);
+
+for(let i = 0; i < sectionEl.length; i++) {
+  console.log(sectionEl[i]);
+}
